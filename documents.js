2d92ce19d9b5688a50a7fe54d2bd6a7c5003d7c2
rtcomp2026 <zhang.chi097@gmail.com>
2026-08-10 15:18:51 -0400

    Delete documents.js

--- documents.js
+++
@@ -1,9 +0,0 @@
-export function saveDocument(name, content) {
-    const docs = JSON.parse(localStorage.getItem("docs") || "{}");
-    docs[name] = content;
-    localStorage.setItem("docs", JSON.stringify(docs));
-}
-
-export function loadDocuments() {
-    return JSON.parse(localStorage.getItem("docs") || "{}");
-}
